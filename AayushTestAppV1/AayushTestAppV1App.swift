947652d3c64b0f677cb291d94ccabe1f15083fa6
Aayush Agarwal <reach.aayush@gmail.com>
2025-12-02 19:25:48 -0500

    Initial Commit

---
+++ AayushTestAppV1/AayushTestAppV1App.swift
@@ -0,0 +1,17 @@
+//
+//  AayushTestAppV1App.swift
+//  AayushTestAppV1
+//
+//  Created by Aayush Agarwal on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AayushTestAppV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
